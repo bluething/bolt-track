468bebbf70d0e98dbc6eecae4616dccee895a2a6
Habib Ali Machpud <habib.machpud@gmail.com>
2025-06-25 21:27:58 +0700

    init

---
+++ src/main/java/io/github/bluething/java/bolttrack/BoltTrackApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.java.bolttrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoltTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoltTrackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/bluething/java/bolttrack/BoltTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.java.bolttrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoltTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
